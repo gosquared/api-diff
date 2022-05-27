d22c46378c729f9c9ee37434ce36d8dd9ca7e75d
Geoff Wagstaff <geoff@gosquared.com>
2022-05-27 08:47:56 +0000

    host args

--- README.md
+++ README.md
@@ -30,11 +30,17 @@ Requests are curl arguments, fed right into `curl`
 
 Run:
 ```bash
+# apply to every request
+export CURL_ARGS='-s'
+# host a only
+export HOST_A_ARGS='-H "Authorization: bearer 1234"'
+# host b only
+export HOST_B_ARGS='-H "Authorization: bearer 5678"'
+
 zx dist/run.js reqs.txt \
   --host-a "https://api.gosquared.com" \
   --host-b "https://api-2.gosquared.com"
 ```
-
 
 # Reference
 ```bash
--- run.ts
+++ run.ts
@@ -10,11 +10,19 @@
 */
 
 import {$, argv, question} from 'zx'
-import { createReadStream } from 'fs';
+import { createReadStream, writeFileSync } from 'fs';
 import { createInterface } from 'readline';
+import { execSync } from 'child_process';
 
 if (argv.help) {
   console.log(`
+  # apply to every request
+  export CURL_ARGS='-s'
+  # host a only
+  export HOST_A_ARGS='-H "Authorization: bearer 1234"'
+  # host b only
+  export HOST_B_ARGS='-H "Authorization: bearer 5678"'
+
   npx api-diff reqs.txt --host-a test.com --host-b test2.com
   `);
   process.exit();
@@ -29,40 +37,72 @@ function open_file(path: string) {
   });
 }
 
-function req(host: string, line: string, dest: string) {
-  const opts = line.replace('{HOST}', host).split(' ');
-  const args = [ '-s', ...opts ];
-  return $`curl ${args} > ${dest}`;
+function jsonFormat(str: string) {
+  return JSON.stringify(JSON.parse(str), null, 2);
 }
 
+function req(host: string, line: string, dest: string, extraCurlArgs: string = '') {
+  let args = line.replace('{HOST}', host);
+
+  if (extraCurlArgs) args += ` ${extraCurlArgs}`;
+
+  const cmd = `curl ${args}`;
+  console.log(cmd)
+  const result = execSync(cmd, { encoding: 'utf8' });
+  const formatted = jsonFormat(result);
+  writeFileSync(dest, formatted);
+}
+
+// argv._: [ '.bin/api-diff', 'reqs.txt' ]
 console.log(argv)
 
 const hostA = argv['host-a'];
 const hostB = argv['host-b'];
+
+const curlArgs = process.env.CURL_ARGS || '';
+const hostAArgs = process.env.HOST_A_ARGS || '';
+const hostBArgs = process.env.HOST_B_ARGS || '';
+
+console.log({
+  curlArgs,
+  hostAArgs,
+  hostBArgs
+});
 
 if (!hostA || !hostB) {
   console.log('--host-a and --host-b are required');
   process.exit(1);
 }
 
+async function handleLine(line: string): Promise<void> {
+  // e.g. line:
+    //  -XGET -H 'Content-Type: application/json' {HOST}/profiles/1?project_id=1 -d '{}'
+    console.log(line);
+
+    let hostACurlArgs = '';
+    let hostBCurlArgs = '';
+
+    if (curlArgs) {
+      hostACurlArgs = `${curlArgs} ${hostAArgs}`;
+      hostBCurlArgs = `${curlArgs} ${hostBArgs}`;
+    }
+
+    req(hostA, line, './a.result.json', hostACurlArgs),
+    req(hostB, line, './b.result.json', hostBCurlArgs)
+
+    await $`npx json-diff --color ./a.result.json ./b.result.json`;
+
+    const response = await question('Continue c, retry r, quit q [c] ');
+
+    if (response === 'r') return handleLine(line);
+    if (response === 'q') process.exit(0);
+}
+
 async function main() {
   const lines = open_file(argv._[1]);
 
   for await (const line of lines) {
-    // e.g. line:
-    //  -XGET -H 'Content-Type: application/json' {HOST}/profiles/1?project_id=1 -d '{}'
-    console.log(line);
-
-    await Promise.all([
-      req(hostA, line, './a.result.json'),
-      req(hostB, line, './b.result.json')
-    ]);
-
-    await $`npx json-diff --color ./a.result.json ./b.result.json`;
-
-    const response = await question('Continue y/n? [y] ');
-
-    if (response === 'n') process.exit(0);
+    await handleLine(line);
   }
 }
 
